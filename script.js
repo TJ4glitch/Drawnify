c98b864eb16c902e89e19cf796d27d26715b62d9
TJ4glitch <sajimolphilip1263@gmail.com>
2025-10-18 13:36:14 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+const slides = document.querySelectorAll('.slide');
+let currentSlide = 0;
+
+function showSlide(index) {
+  slides.forEach((slide, i) => {
+    slide.classList.remove('active');
+    if (i === index) slide.classList.add('active');
+  });
+}
+
+function nextSlide() {
+  currentSlide = (currentSlide + 1) % slides.length;
+  showSlide(currentSlide);
+}
+
+// Auto slide every 5 seconds
+setInterval(nextSlide, 5000);
